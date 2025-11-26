fa1b8d271cdf70fea50ac3efd16349bddd77c106
Pradana098 <pradanadimas534@gmai.com>
2025-11-26 18:39:09 +0700

    gegabutan sendiri

---
+++ assets/style/style.js
@@ -0,0 +1,4 @@
+// Simple JavaScript for mobile menu toggle
+        document.querySelector('.hamburger').addEventListener('click', function() {
+            document.querySelector('.nav-links').classList.toggle('active');
+        });
